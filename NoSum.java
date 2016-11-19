193b8b65bb022f6b375033b78c275588cad779a0
tashi tsering <tatsering54@gmail.com>
2016-11-20 04:35:09 +0530

    Add files via upload

---
+++ NoSum.java
@@ -0,0 +1,13 @@
+class NoSum {
+ 
+	public static void main(String [] args){
+		
+	int num1=10,num2=20;
+	int num3=num1-(-num2);
+	System.out.println("Addition of two num1 and num2 :"  + num3);
+
+
+	
+	}
+	
+}
